8dbf31eee89f0c09509170f2ade138d1e0e56596
avei <aveeIsPro@gmail.com>
2026-06-16 12:25:45 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,24 +1,21 @@
 #include <Windows.h>
 
-uintptr_t ModuleBase = (uintptr_t)GetModuleHandleA(0); // Get RobloxPlayer.exe Module Handle (0 = Parent Window Handle)
+uintptr_t base = (uintptr_t)GetModuleHandleA(0); // get roblox base, since we are injected to it, we put 0, which indicates the exe we are in, in this case RobloxPlayerBeta.exe
 
-using Print = DWORD64(__cdecl*)(unsigned int Type, const char* Message, ...); // Type for print
-Print PrintConsole = reinterpret_cast<Print>(ModuleBase + 0x1DB7F90); // Actual print, calling Print Offset, on the Module Base (Roblox)
+using Print = DWORD64(__cdecl*)(unsigned int, const char*, ...);
+Print r_print = reinterpret_cast<Print>(base + 0x1E08380); // needs updating uwu
 
-void StartPrinting()
+void start()
 {
-    int Seconds = 1; // How many seconds is 1 second (yes.)
-    while (true) { // Loops the print
-        PrintConsole(1LL, "injected for %ds", Seconds); // Prints how many seconds active
-        Seconds++; // add 1 second each time
-        Sleep(1000); // Slows down the loop to run once per second
-                    // (1000 milliseconds = 1 second)
-    }
+    r_print(0, "injected"); // default print
+    r_print(1, "injected"); // info print
+    r_print(2, "injected"); // warning print
+    r_print(3, "injected"); // error print
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) { // EntryPoint
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) { // If DLL is injected/attached then do..
-        StartPrinting(); // Starting our printsploit
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        start();
     }
     return TRUE;
 }

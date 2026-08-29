519e3fb27f51108fec8dd93488c3aa7bede8f08b
avei <aveeIsPro@gmail.com>
2026-08-29 13:49:27 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
 #define rebase(x) (x + reinterpret_cast<uintptr_t>GetModuleHandleA(0))
 
-const uinptr_t Print = 0x834D90; // needs updating
+const uint64_t Print = 0x834D90; // needs updating
 using r_print_t = int(__fastcall*)(unsigned int, const char*, ...);
 r_print_t r_print = reinterpret_cast<r_print_t>(Print);
 
